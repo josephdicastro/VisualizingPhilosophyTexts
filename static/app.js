4d8340d4025a64dc237724f0da6c1b22b304f983
josephdicastro <joe.dicastro@gmail.com>
2020-03-26 13:59:54 -0400

    updated index.html, app.js, datacollection.ipynb

--- static/app.js
+++ static/app.js
@@ -1,5 +1,6 @@
 
-const selectMenu = d3.select('#searchMenu')
+const selectMenu1 = d3.select('#selectMenu1')
+const selectMenu2 = d3.select('#selectMenu2')
 
 const margin = {
     top: 30,
@@ -7,15 +8,19 @@ const margin = {
     bottom: 30,
     left: 50
 };
-const width = 700 - margin.left - margin.right;
-const height = 450 - margin.top - margin.bottom;
+const width = 500 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-const g = d3.select("svg")
+const g1 = d3.select("#cloud1")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .style('border', '1px solid black');
 
-d3.json("static/kant.json").then(data => { 
+const g2 = d3.select("#cloud2")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+
+d3.json("static/wordcloud.json").then(data => { 
     searchTerms = []
     searchTerms.push('...Select Text...')
     titles = Object.keys(data)
@@ -23,9 +28,8 @@ d3.json("static/kant.json").then(data => {
         searchTerms.push(title)
     })
 
-    console.log(searchTerms)
 
-    selectMenu.selectAll("option")
+    selectMenu1.selectAll("option")
                 .data(searchTerms)
                 .enter()
                     .append("option")
@@ -33,19 +37,32 @@ d3.json("static/kant.json").then(data => {
                     .html((d) => d)
                 .exit().remove();
 
-    selectMenu.on('change', function() {
+    selectMenu1.on('change', function() {
         selectedText = d3.event.target.value;
-        showText(selectedText);
+        showText(selectedText,"menu1");
+    })
+
+    selectMenu2.selectAll("option")
+    .data(searchTerms)
+    .enter()
+        .append("option")
+        .attr("value", (d) => d)
+        .html((d) => d)
+    .exit().remove();
+
+    selectMenu2.on('change', function() {
+    selectedText = d3.event.target.value;
+    showText(selectedText, "menu2");
     })
     
     wordCloudData = []
 
     let color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    makeCloud();
+    makeCloud1();
+    makeCloud2();
 
-    function showText(selectedText) {
-
+    function showText(selectedText, menuNum) {
 
 
         if (selectedText !== '...Select Text...') {
@@ -57,36 +74,71 @@ d3.json("static/kant.json").then(data => {
                 .domain([minVal,maxVal])
                 .range([10,50])
 
-            console.log(minVal,maxVal)
             wordCloudData.length = 0;
             wordValues.forEach(function(word) {
                 word.text = word.text
                 word.value = scaleType(word.value)
-                wordCloudData.push(word)
-                console.log(word)
+                wordCloudData.push(word) 
             })
             
-            makeCloud();
+            console.log(menuNum)
+            if (menuNum === 'menu1') {
+                makeCloud1();
+            } else {
+                makeCloud2();
+            }
+            
         
         }
     }
 
-    function makeCloud() {
-        let wordCloudlayout = d3.layout.cloud()
-            .size([700, 450])
+    function makeCloud1() {
+        let wordCloudlayout1 = d3.layout.cloud()
+            .size([500, 400])
             .words(wordCloudData)
             .fontSize((d)=>d.value)
             .font("Gill Sans MT")
             .padding(5)
-            .on("end", draw);
+            .on("end", draw1);
     
-        wordCloudlayout.start();
+        wordCloudlayout1.start();
 
-        function draw(words) {
-            d3.select("svg").html("")
-            d3.select("svg")
+        function draw1(words) {
+            d3.select("#cloud1").html("")
+            d3.select("#cloud1")
                 .append("g")
-                .attr("transform", "translate(" + wordCloudlayout.size()[0] / 2 + "," + wordCloudlayout.size()[1] / 2 + ")")
+                .attr("transform", "translate(" + wordCloudlayout1.size()[0] / 2 + "," + wordCloudlayout1.size()[1] / 2 + ")")
+                .selectAll("text")
+                .data(words)
+                .enter()
+                    .append("text")
+                    .text((d) => d.text)
+                    .style("font-size", (d) => (d.value)  + "px")
+                    .style("font-family", (d) => d.font)
+                    .style("fill", (d, i) => color(i))
+                    .attr("text-anchor", "middle")
+                    .attr("transform", (d) => "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")")
+                .exit()
+                    .remove()
+        }
+    }
+
+    function makeCloud2() {
+        let wordCloudlayout2 = d3.layout.cloud()
+            .size([500, 400])
+            .words(wordCloudData)
+            .fontSize((d)=>d.value)
+            .font("Gill Sans MT")
+            .padding(5)
+            .on("end", draw2);
+    
+        wordCloudlayout2.start();
+
+        function draw2(words) {
+            d3.select("#cloud2").html("")
+            d3.select("#cloud2")
+                .append("g")
+                .attr("transform", "translate(" + wordCloudlayout2.size()[0] / 2 + "," + wordCloudlayout2.size()[1] / 2 + ")")
                 .selectAll("text")
                 .data(words)
                 .enter()
